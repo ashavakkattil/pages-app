0dfc13c49be67dfc0ad98dd357e448e028c479e4
Asha <asha.vakkattil@gmail.com>
2020-04-10 17:23:08 +0530

    Reset button functionality added

--- pages/src/pages/Index.vue
+++ pages/src/pages/Index.vue
@@ -4,7 +4,7 @@
       <q-card flat bordered class='my-card'>
         <q-card-section class='my-card-section'>
         <div class="row"> <h5 class='col-6 q-my-md float-left'>Add Page</h5>
-        <q-toggle class="col-6 float-right publish-button" v-model="page.published"></q-toggle>
+        <q-toggle class="col-6 float-right publish-button" v-model="page.published">Publish Page</q-toggle>
         </div>
         <div>
         <q-select
@@ -18,11 +18,11 @@
           ></q-select>
           <q-input label='Add title to the page' outlined dense v-model='page.page_title' class='q-my-md'></q-input>
           <q-input label='Page Url' outlined dense v-model='page.page_url' class='q-my-md'></q-input>
-          <q-editor placeholder='Type...' v-model='page.page_description'></q-editor></div>
+          <q-editor placeholder='Type...' v-model='page_description'></q-editor></div>
         </q-card-section>
         <q-card-actions align='center'>
           <q-btn outline class='q-ma-sm' color='primary' @click='addPage'>Add Page</q-btn>
-          <q-btn outline class='q-ma-sm' color='secondary' @click='resetSelect'>Cancel</q-btn>
+          <q-btn outline class='q-ma-sm' color='secondary' @click='resetSelect'>Reset</q-btn>
         </q-card-actions>
       </q-card>
     </div>
@@ -38,21 +38,26 @@ export default {
         { value: 'about', name: 'About Us' },
         { value: 'terms', name: 'Terms & Conditions' }
       ],
+      page_description: '',
       page: {
         page_type: '',
         page_title: '',
         page_description: '',
         page_url: '',
         published: false
-      },
-      test: 'asha'
+      }
     }
   },
   methods: {
     resetSelect () {
-      this.pageType = ''
+      this.page_description = ' '
+      this.page.page_type = ''
+      this.page.page_title = ''
+      this.page.page_url = ''
+      this.page.published = false
     },
     async addPage () {
+      this.page.page_description = this.page_description
       this.page.page_type = this.page.page_type.value
       await this.$axios
         .post('/', this.page)
